f575c06d9ed4c19bd795b8ff90379e9421b74d69
surayya <129226184+karima12415@users.noreply.github.com>
2023-06-19 00:16:50 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+function App(){
+  return (
+    <>
+    <div class="border">
+      <div class="image">
+         <img src={require('./img.jpg')} alt="img "/>
+         <h1>Salem Attia Karima</h1>
+         <p>hello, my name is karima </p>
+         <p>I live in Algeria </p>
+      </div>
+    </div>
+    </>
+  );
+}
+export default App;
